7e958c35652374f7311e62f7937c963b142be7e2
kchafla <kchaflam@fp.insjoaquimmir.cat>
2021-05-12 18:53:10 +0200

    Canvis

--- Projecte/resources/views/reproductor.blade.php
+++ Projecte/resources/views/reproductor.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <meta name="room" content="{{ $sala }}">
+    <meta name="room" content="{{ $sala->id }}">
     <meta name="chat" content="{{ $chat }}">
     <meta name="lastvideo" content="{{ $video }}">
     <meta name="newvideo" content="{{ url('sala/'.$sala.'/video') }}">
@@ -19,20 +19,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-dark border-b border-gray-200 text-light backgrounDark">
-                    <!---->
-                    Pagina de videos
+                    <div class="offset-md-3 col-md-6 text-center">
+                        <h1>{{ $sala->name }}</h1>
+                    </div>
                     
                     <form id="buscar_form">
                         <div class="row">
-                            <input type="text" name="nom" id="buscar_nom" class="col-md-11 text-dark">
-                            <button class="col-md-1 bg-primary">🔎</button>
+                            <input type="text" name="nom" id="buscar_nom" class="offset-md-1 col-md-9 text-dark" placeholder="Buscar un video...">
+                            <button id="buscar-video" class="col-md-1 btn btn-primary">Buscar</button>
                         </div>
                     </form>
                     
                     <div class="row">
                         <!--Apartat del video seleccionat-->
-                        <div class="col-md-8" height="2500" style="margin-top: 15px;">
-                                <div class="col-md-12" style="padding: 10px; height: 360px">
+                        <div class="col-md-8" style="margin-top: 15px;">
+                                <div class="col-md-12" style="padding: 10px; height: 460px">
                                     <div id="reproductor" style="width: 100%; height: 100%"></div>
                                 </div>
                         </div>
@@ -43,9 +44,9 @@
                                 <div id="chat" class="backgrounChat text-black">
                                 
                                 </div>
-                                <form action="{{ url('sala/'.$sala.'/mensaje/'.$chat) }}" id="message_form" class="bg-primary">
+                                <form action="{{ url('sala/'.$sala.'/mensaje/'.$chat) }}" id="message_form">
                                     <input type="text" id="message" placeholder="Escriu un nou missatge..." class="col-md-10 text-dark">
-                                    <button class="col-md-1">🔎</button>
+                                    <button class="col-md-2 btn btn-primary">🔎</button>
                                 </form>
                             </div>
                         </div>
